2df5a3d06fbc769adbf78bb8d3a0edaa4d265413
Scott Rippey <scott.william.rippey@gmail.com>
2013-04-27 01:15:43 -0700

    DataBind - added data-bind-repeat

--- js/Element.dataBind.js
+++ js/Element.dataBind.js
@@ -1,31 +1,53 @@
 (function(){
 	Element.implement({
-		dataBind: function(data, helper) {
-			var el = this;
-
-			var dataBindings = el.retrieve('$dataBindings');
-			if (!dataBindings) {
-				dataBindings = createDataBindings(el);
-				el.store('$dataBindings', dataBindings);
-			}
-
-			dataBindings.each(function(dataBinding) {
-				dataBinding(data, helper);
-			});
+		/**
+		 *
+		 * @param {*} data
+		 * @param {*} [helper]
+		 * @param {Number} [index]
+		 * @param {Number} [remaining]
+		 * @returns {Element}
+		 */
+		dataBind: function(data, helper, index, remaining) {
+			var dataBinding = getDataBinding(this);
+			dataBinding(data, helper, index, remaining);
+			return this;
 		}
 	});
 
-	function createDataBindings(el) {
+	function getDataBinding(el) {
+		var cachedDataBinding = el.retrieve('$dataBinding');
+		if (cachedDataBinding) {
+			return cachedDataBinding;
+		}
+
+		var dataBindings = [];
+
+		var repeater;
+		while (repeater = el.getElement('[data-bind-repeat]')) {
+			var repeaterBinding = createDataRepeaterFunction(repeater);
+			dataBindings.push(repeaterBinding);
+			repeater.set('data-bind-repeat', null);
+		}
+
 		var dataBoundEls = el.getElements('[data-bind]');
 		if (el.match('[data-bind]'))
 			dataBoundEls.push(el);
 
-		var dataBindings = dataBoundEls.map(function(el) {
-			var dataBindAttribute = el.get('data-bind');
-			var compiled = createDataBindingFunction(dataBindAttribute);
-			return compiled.bind(el);
+		dataBoundEls.each(function(el) {
+			var dataBindingFunction = createDataBindingFunction(el);
+			dataBindings.push(dataBindingFunction);
 		});
-		return dataBindings;
+
+		var dataBinding = function(data, helper, index, remaining) {
+			Array.each(dataBindings, function(binding) {
+				binding(data, helper, index, remaining);
+			});
+		};
+
+		el.store('$dataBinding', dataBinding);
+
+		return dataBinding;
 	}
 
 
@@ -36,14 +58,49 @@
 	 *      Example: data-bind="this.set('text', 'Hello');"
 	 * 2. JSON - this is simply a shortcut for this.set(...)
 	 *      Example: data-bind="{ text: 'Hello' }"
-	 * @param dataBindAttribute
+	 * @param el
 	 * @returns {Function}
 	 */
-	function createDataBindingFunction(dataBindAttribute) {{
+	function createDataBindingFunction(el) {{
+		var dataBindAttribute = el.get('data-bind');
 		if (dataBindAttribute.match(/^\s*\{/))
 			dataBindAttribute = "this.set(#)".replace("#", dataBindAttribute)
 		}
-		var dataBindFn = new Function('data', 'helper', dataBindAttribute);
-		return dataBindFn;
+		var dataBindFn = new Function('data', 'helper', 'index', 'remaining', dataBindAttribute);
+		return dataBindFn.bind(el);
+	}
+
+	function createDataRepeaterFunction(repeater) {
+		var dataBindRepeatAttribute = repeater.get('data-bind-repeat');
+		var functionText = "return #;".replace("#", dataBindRepeatAttribute);
+		var getItems = new Function('data', 'helper', 'index', 'remaining', functionText);
+
+		var repeaterTemplate = repeater.getChildren().dispose();
+		var repeaterItems = [];
+		var repeaterBinding = function(data, helper, index, remaining) {
+			var items = getItems(data, helper, index, remaining);
+			for (var itemIndex = 0, length = Math.max(items.length, repeaterItems.length); itemIndex < length; itemIndex++) {
+				if (itemIndex >= items.length) {
+					repeaterItems.pop().dispose();
+					continue;
+				}
+
+				var repeaterItem;
+				if (itemIndex < repeaterItems.length) {
+					repeaterItem = repeaterItems[itemIndex];
+				} else {
+					var clonedElements = new Elements(repeaterTemplate.map(function(el) { return el.clone(); }));
+					repeaterItem = clonedElements;
+					repeater.adopt(repeaterItem);
+					repeaterItems.push(repeaterItem);
+				}
+
+				var item = items[itemIndex], itemsRemaining = (items.length - 1 - itemIndex);
+
+				repeaterItem.each(function(el) { el.dataBind(item, helper, itemIndex, itemsRemaining); });
+			}
+		};
+
+		return repeaterBinding;
 	}
 })();
--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ window.addEvent('domready', function(){
 			,isFavorite: true
 			,imageUrl: "http://lorempixel.com/400/300"
 			,description: "Bacon ipsum risket chuck tail ball tip kielbasa beef ham swine strip steak fatback meatloaf pork belly chicken shoulder. Capicola frankfurter boudin chicken, t-bone pork chop salami ham."
+			,tags: [ 'Bacon', 'ipsum', 'risket', 'chuck', 'tail', 'ball' ]
 		}
 		,
 		{
@@ -13,6 +14,7 @@ window.addEvent('domready', function(){
 			,isFavorite: false
 			,imageUrl: "http://lorempixel.com/160/90"
 			,description: "Pork ball tip shank doner pancetta chicken. Pork loin swine pastrami rump drumstick ribeye kielbasa venison doner sausage spare ribs ham hock tongue."
+			,tags: [ 'Pork', 'ball', 'tip' ]
 		}
 		,
 		{
@@ -20,6 +22,7 @@ window.addEvent('domready', function(){
 			,isFavorite: true
 			,imageUrl: "http://lorempixel.com/300/400"
 			,description: "Jerky short loin chuck hamburger jowl tail. Kielbasa turducken leberkas, frankfurter meatloaf prosciutto pork loin biltong hamburger beef pig. Chuck spare ribs pig, ham hock boudin shank bacon turkey pork chop pork loin kielbasa ball tip flank."
+			,tags: [ 'Jerky', 'short', 'loin', 'chuck', 'hamburger', 'jowl', 'tail' ]
 		}
 		,
 		{
@@ -27,6 +30,7 @@ window.addEvent('domready', function(){
 			,isFavorite: false
 			,imageUrl: "http://lorempixel.com/300/300"
 			,description: "Sirloin pancetta jerky doner fatback corned beef pork loin. Meatball strip steak venison flank andouille ground round pancetta meatloaf chuck prosciutto filet mignon tenderloin."
+			,tags: [ 'Sirloin', 'pancetta', 'jerky', 'doner', 'fatback' ]
 		}
 	];
 
