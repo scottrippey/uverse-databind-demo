815521500ce8e0d7364a1a6526fb67101156e1f1
Scott Rippey <scott.william.rippey@gmail.com>
2013-04-26 21:01:18 -0700

    DataBind - added shorthand JSON support

--- js/Element.dataBind.js
+++ js/Element.dataBind.js
@@ -1,13 +1,49 @@
-Element.implement({
-	dataBind: function(data, helper) {
-		var dataBoundEls = this.getElements('[data-bind]');
-		if (this.match('[data-bind]'))
-			dataBoundEls.push(this);
+(function(){
+	Element.implement({
+		dataBind: function(data, helper) {
+			var el = this;
 
-		dataBoundEls.each(function(el) {
-			var command = el.get('data-bind');
-			var compiled = new Function('data','helper',command);
-			compiled.call(el, data, helper);
+			var dataBindings = el.retrieve('$dataBindings');
+			if (!dataBindings) {
+				dataBindings = createDataBindings(el);
+				el.store('$dataBindings', dataBindings);
+			}
+
+			dataBindings.each(function(dataBinding) {
+				dataBinding(data, helper);
+			});
+		}
+	});
+
+	function createDataBindings(el) {
+		var dataBoundEls = el.getElements('[data-bind]');
+		if (el.match('[data-bind]'))
+			dataBoundEls.push(el);
+
+		var dataBindings = dataBoundEls.map(function(el) {
+			var dataBindAttribute = el.get('data-bind');
+			var compiled = createDataBindingFunction(dataBindAttribute);
+			return compiled.bind(el);
 		});
+		return dataBindings;
 	}
-});
+
+
+	/**
+	 * Parses the data-bind attribute into an actual function.
+	 * This method supports 2 syntaxes:
+	 * 1. JavaScript - simply write full javascript code
+	 *      Example: data-bind="this.set('text', 'Hello');"
+	 * 2. JSON - this is simply a shortcut for this.set(...)
+	 *      Example: data-bind="{ text: 'Hello' }"
+	 * @param dataBindAttribute
+	 * @returns {Function}
+	 */
+	function createDataBindingFunction(dataBindAttribute) {{
+		if (dataBindAttribute.match(/^\s*\{/))
+			dataBindAttribute = "this.set(#)".replace("#", dataBindAttribute)
+		}
+		var dataBindFn = new Function('data', 'helper', dataBindAttribute);
+		return dataBindFn;
+	}
+})();
