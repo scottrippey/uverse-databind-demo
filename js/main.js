1d038030da9c433fdd22c28147f3efd03d7887bb
Scott Rippey <scott.william.rippey@gmail.com>
2013-04-26 17:26:02 -0700

    DataBind - Added initial implementation of the dataBind method

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
+window.addEvent('domready', function(){
+
+	var flyoutDataItems = [
+		{
+			title: "Bacon andouille ribeye frankfurter"
+			,isFavorite: true
+		}
+		,
+		{
+			title: "Ham biltong cow meatball "
+			,isFavorite: false
+		}
+		,
+		{
+			title: "Tongue fatback cow bresaola"
+			,isFavorite: true
+		}
+		,
+		{
+			title: "Prosciutto short loin frankfurter"
+			,isFavorite: false
+		}
+	];
+
+	var index = 0;
+	function showNextData() {
+		var flyoutData = flyoutDataItems[index++ % flyoutDataItems.length];
+		var flyout = $$('.flyout');
+		updateFlyout(flyout, flyoutData);
+	}
+
+	function updateFlyout(flyout, flyoutData) {
+		flyout.dataBind(flyoutData);
+	}
+
+	setInterval(showNextData, 2000);
+	showNextData();
+
+
+});
 
